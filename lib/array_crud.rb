a1d11cf2e2fccdd2cb7ee566c42d497e230b1175
RonakAgarwal <agarwalronak02@gmail.com>
2018-06-29 19:11:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -49,5 +49,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  return array[5]
+
 
 end
